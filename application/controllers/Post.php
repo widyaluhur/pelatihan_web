b95e163d5dc0277bce7e874729ec28f8c9f5e89b
Widya Luhur Wicaksono <luhurwidya1@gmail.com>
2021-02-21 23:00:19 +0700

    perubahan controller post

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -43,7 +43,7 @@ class Post extends CI_Controller
 
         $this->load->library('pagination');
 
-        $config['base_url'] = 'http://localhost/ci3kp/post/index';
+        $config['base_url'] = 'https://pelatihan-web.herokuapp.com/post';
 
         if ($this->session->userdata('keyword') == false) {
 

d519163bb9e80490d5bc4747f36fc02405d23818
Widya Luhur Wicaksono <luhurwidya1@gmail.com>
2021-06-27 17:14:43 +0700

    fourth-commit

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,8 @@
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
         <h1 class="display-4"><?= $nama; ?></h1>
-        <p class="lead">20503241012 \nPendidikan Teknik Mesin \n Kelas A</p>
+        <p class="lead">20503241012</p>
+        <p class="lead">Pendidikan Teknik Mesik</p>
+        <p class="lead">Kelas A</p>
     </div>
 </div>

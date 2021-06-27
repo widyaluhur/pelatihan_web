ceb8cd2af02cbea731996a195148a92ada1e7bb1
Widya Luhur Wicaksono <luhurwidya1@gmail.com>
2021-06-27 17:18:45 +0700

    five-commit

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h1 class="display-4"><?= $nama; ?></h1>
         <p class="lead">20503241012</p>
-        <p class="lead">Pendidikan Teknik Mesik</p>
-        <p class="lead">Kelas A</p>
+        <p class="lead">Pendidikan Teknik Mesin</p>
+        <p class="lead">Kelas Asu we yud</p>
     </div>
 </div>

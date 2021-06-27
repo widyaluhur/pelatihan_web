1d1a1469e3cfec86e633db0dde24aba9a79ad39d
Widya Luhur Wicaksono <luhurwidya1@gmail.com>
2021-06-27 17:11:42 +0700

    third-commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
  <?php
    class Home extends CI_Controller
    {
-      public function index($nama = 'Yudhan')
+      public function index($nama = 'Yudhan Rahmat Afriawan')
       {
          $data['judul'] = 'Halaman Home';
          $data['nama'] = $nama;
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
         <h1 class="display-4"><?= $nama; ?></h1>
-        <p class="lead">123180079</p>
+        <p class="lead">20503241012 \nPendidikan Teknik Mesin \n Kelas A</p>
     </div>
 </div>
